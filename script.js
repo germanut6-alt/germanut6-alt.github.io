011c652601fe53bddcbed6ab97c59cf401a83bb9
germanut6-alt <germanut6@gmail.com>
2026-07-27 13:52:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const telegramApp = window.Telegram.WebApp;
+
+telegramApp.ready();
+telegramApp.expand();
+
+telegramApp.showAlert(
+    "927508173" + telegramApp.initDataUnsafe.user.id
+);
+
+const allowedUserId = "927508173";
+const currentUserId = telegramApp.initDataUnsafe.user?.id;
+
+if (currentUserId !== allowedUserId) {
+    document.body.innerHTML = `
+        <main>
+            <h1>Access denied</h1>
+            <p>This test app is private.</p>
+        </main>
+    `;
+
+    throw new Error("Access denied");
+}
+
+
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
     manifestUrl: "https://germanut6-alt.github.io/tonconnect-manifest.json"
 });
